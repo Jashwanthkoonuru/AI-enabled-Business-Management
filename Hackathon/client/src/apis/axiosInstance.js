f3a8c4a5cfa8b1396b11905284d071a383e7689e
Jashwanthkoonuru <jashwanthjay@gmail.com>
2026-01-30 11:20:50 +0530

    Update API URL to production server

--- Hackathon/client/src/apis/axiosInstance.js
+++ Hackathon/client/src/apis/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1";
+const API_URL = "http://35.154.121.57:8085/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
